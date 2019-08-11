2f3ed1a418c51975c3944a49e428a57613cb5788
Pedro Gonzalez <pdro74@hotmail.com>
2019-08-11 19:01:23 +0200

    add test request

--- routes/api/gigs.js
+++ routes/api/gigs.js
@@ -5,4 +5,16 @@ let Gig = require('../../models/gig')
 
 // TODO: CRUD for gigs
 
+// GET all gigs:
+router.get('/', (req, res) => {
+    // Gig.find((err, gigsResult) => {
+    //     if (err) return res.json({ error: err})
+    //     console.log(gigsResult)
+    //     res.json(gigsResult)
+    // })
+
+    console.log("Get all gigs request OK");
+
+})
+
 module.exports = router;
